0ccc74d83f373df0648d8498070b885f0e3f212b
gonzalovazquez <gonzalovazquez010@gmail.com>
2016-11-28 18:52:28 -0500

    Added mock data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import searches from './Comparison.json';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      value: "test",
+      value: 'test',
     };
+    console.log(searches);
   }
   render() {
+    const listItems = searches.map((elem, key) =>
+          <li key={key}>{elem.id}</li>
+    );
     return (
       <div className="App">
         <div className="App-header">
@@ -19,6 +24,7 @@ class App extends Component {
         <p className="App-intro">
           {this.state.value}
         </p>
+        <ul>{listItems}</ul>
       </div>
     );
   }
