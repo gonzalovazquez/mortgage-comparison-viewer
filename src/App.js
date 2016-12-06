32d4f7d00ef40e3162ce083b641f6b5da95d972d
gonzalovazquez <gonzalovazquez010@gmail.com>
2016-12-06 13:32:08 -0500

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import searches from './Comparison.json';
+import Widget from './widget/widget';
 import { Grid, Row, Col, Button } from 'react-bootstrap';
 import './App.css';
 
@@ -12,33 +12,9 @@ class App extends Component {
     };
     this.selectedWidget = (e) => {
       console.log('this is:', this);
-    }
+    };
   }
   render() {
-    const listItems = searches.map((listing, key) =>
-          <Col xs={6} md={4} key={key}>
-            <div className="widget" onClick={this.selectedWidget}>
-                <h3>Search # {listing.id} </h3>
-                <img src={listing.image} alt="house" width="100%" />
-                <p>
-                  Loan Balance: 
-                  <strong>
-                      ${listing.loan_balance}
-                  </strong>
-                </p>
-                <p>
-                  Interest Rate: <strong>{listing.interest_rate}%</strong>
-                </p>
-                <p>
-                  Loan term: <strong>{listing.term} years</strong>
-                </p>
-                <p>Period: <strong>{listing.period}</strong>
-                </p>
-                <p><a href={listing.link}>Reference</a></p>
-                <p className="amount">${listing.monthly_payments} per month</p>
-              </div>
-          </Col>
-    );
     return (
       <div className="App">
         <div className="App-header">
@@ -46,13 +22,11 @@ class App extends Component {
         </div>
           <Grid>
             <Row className="show-grid">
-               <Col xs={12} md={12} className="ta-r">
+               <Col xs={12} md={12} className="tar">
                 <Button bsStyle="primary" className="compare-btn">Compare</Button>
                </Col>
             </Row>
-            <Row className="show-grid">
-              {listItems}
-            </Row>
+              <Widget />
           </Grid>
       </div>
       
